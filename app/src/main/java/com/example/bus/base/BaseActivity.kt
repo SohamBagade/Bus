bace885968b244704e4e716ebf4d9bfa41fc145e
Prasad Pasalkar <prasad.p@2c2pexternal.com>
2020-02-13 17:39:58 +0530

    Basic initialization Complete

---
+++ app/src/main/java/com/example/bus/base/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.example.travelvoyage.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity() {
+
+
+
+}
---
+++ app/src/main/java/com/example/bus/base/BaseFragment.kt
@@ -0,0 +1,8 @@
+package com.example.travelvoyage.base
+
+import androidx.fragment.app.Fragment
+
+open class BaseFragment  : Fragment(){
+
+
+}
